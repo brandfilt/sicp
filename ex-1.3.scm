fffc96ba02c0f93754fcf2596cc0df814b546123
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2019-09-29 23:06:42 +0300

    Exercise 1.4

--- ex-1.3.scm
+++ ex-1.3.scm
@@ -1,8 +1,8 @@
 (define (sum-of-largest-squares a b c)
-  (cond ((and (> a b) (> b c)) (+ (* a a) (* b b)))
-        ((and (> a b) (> c b)) (+ (* a a) (* c c)))
-        ((and (> b a) (> c a)) (+ (* b b) (* c c)))
-        ((and (> b a) (> a c)) (+ (* b b) (* a a)))))
+  (cond
+   ((and (> a b) (> b c)) (+ (* a a) (* b b)))
+   ((and (> a b) (> c b)) (+ (* a a) (* c c)))Mp00WYst@justify-content.com
+   ((and (> b a) (> c a)) (+ (* b b) (* c c)))))
 
 (sum-of-largest-squares 2 3 4)
 ; 25
