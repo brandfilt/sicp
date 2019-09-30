6b19126a59550e7b936c76e0396db5df2b8c30b5
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2019-09-30 22:18:36 +0300

    Exercise 1.6

---
+++ ex-1.6.scm
@@ -0,0 +1,36 @@
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(new-if (= 2 3) 0 5)
+; 5
+
+(new-if (= 1 1) 0 5)
+; 0
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+;; (define (sqrt-iter guess x)
+;;   (if (good-enough? guess x)
+;;       guess
+;;       (sqrt-iter (improve guess x)
+;;                  x)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(sqrt 9)
+;Aborting!: maximum recursion depth exceeded
