50582898cdb3f3da6a9eb8d1f9215a74e30a3098
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2019-11-21 00:00:31 +0200

    Solution to 1.16

--- ex-1.16.scm
+++ ex-1.16.scm
@@ -1,7 +1,15 @@
 (define (even? n)
   (= (remainder n 2) 0))
 
-(define (fast-expt b n)
-  (cond ((= n 0) 1)
-        ((even? n) (square (fast-expt b (/ n 2))))
-        (else (* b (fast-expt b (- n 1))))))
+(define (square x) (* x x))
+
+(define (expt b n)
+  (iter-expt b n 1))
+
+(define (iter-expt b n a)
+  (cond ((= n 0) a)
+        ((even? n) (iter-expt (square b) (/ n 2) a))
+        (else (iter-expt b (- n 1) (* a b)))))
+
+(fast-expt 2 16)
+(expt 2 16)
